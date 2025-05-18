48880c6cbaab4ff4eaec254e1b84719f183c695a
Luca Denhez <lucadenhez@icloud.com>
2025-05-17 21:22:57 -0700

    Small mobile tweak

--- src/components/PricingInformation.js
+++ src/components/PricingInformation.js
@@ -16,7 +16,7 @@ export default function PricingInformation() {
     }, [enabled]);
 
     return (
-        <div className="flex flex-col justify-end gap-2">
+        <div className="flex flex-col items-end gap-2">
             <h1 className='font-semibold lg:text-3xl text-xl text-right'>${finalPrice} OBO</h1>
             <Field className="flex flex-col gap-2">
                 <div className="flex items-center gap-4">
